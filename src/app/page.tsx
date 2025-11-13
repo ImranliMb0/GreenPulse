159145f779f66ec9da6b142a87157e8d96298d5d
Mohd Imr <imrmohd126@gmail.com>
2025-11-13 09:06:10 +0000

    can u fix login option i cant sign in or sign up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import { HomeComponent } from '@/components/layout/home-page';
-import { AuthProvider } from '@/hooks/use-auth';
 
 export default function Home() {
-  return (
-    <AuthProvider>
-      <HomeComponent />
-    </AuthProvider>
-  );
+  return <HomeComponent />;
 }
